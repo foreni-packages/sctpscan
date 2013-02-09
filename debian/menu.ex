6767b7c82129df2b1e669fcf88bc50c59f2a4dac
Devon Kearns <dookie@kali.org>
2013-02-09 12:42:03 -0700

    Pre-build commit

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(sctpscan):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="sctpscan" command="/usr/bin/sctpscan"
--- debian/sctpscan.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the sctpscan package
-#
-0 4	* * *	root	[ -x /usr/bin/sctpscan_maintenance ] && /usr/bin/sctpscan_maintenance
